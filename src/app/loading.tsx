560804cfb87b19e3b0a19a4c215da99bffcd863c
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2025-05-29 20:15:17 -0300

    Criando tela de carregamento

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center text-center p-6">
+            <h1 className='text-3xl font-bold text-white mt-7'>Loading...</h1>
+        </div>
+    )
+}
